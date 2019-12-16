b81ecf92d05546043bd189b79871246023a59eba
vcarretero <vcarretero@km3net.de>
2019-12-16 13:59:54 +0100

    Added Decay class

--- LinkDef.h
+++ LinkDef.h
@@ -11,14 +11,15 @@
 #pragma link C++ struct OscProb::EigenPoint+;
 #pragma link C++ class vector<OscProb::NuPath>;
 #pragma link C++ class vector<OscProb::PremLayer>;
-#pragma link C++ class vector<complexD>;
 #pragma link C++ class set<OscProb::EigenPoint>;
+
 
 #pragma link C++ class OscProb::PMNS_Base+;
 #pragma link C++ class OscProb::PMNS_Fast+;
 #pragma link C++ class OscProb::PMNS_NSI+;
 #pragma link C++ class OscProb::PMNS_Sterile+;
 #pragma link C++ class OscProb::PMNS_Deco+;
+#pragma link C++ class OscProb::PMNS_Decay+;
 
 #pragma link C++ class OscProb::PremModel+;
 
