b0656f725646a2c26e355ac0d455ef50606e33c6
Alba Domi <64132984+albadomi91@users.noreply.github.com>
2023-02-19 22:24:32 +0100

    extend PMNS_LIV up to dimension 8

--- PMNS_LIV.cxx
+++ PMNS_LIV.cxx
@@ -1,13 +1,18 @@
-////////////////////////////////////////////////////////////////////////
-//
-// Implementation of oscillations of neutrinos in matter in a
-// three-neutrino framework with LIV.
-//
-// This  class inherits from the PMNS_Fast class
-//
-// \author Nafis R. K. Chowdhury - nrkhanchowdhury\@km3net.de 
-// \author Joao Coelho - jcoelho\@apc.in2p3.fr
-////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////                   
+/// class OscProb::PMNS_LIV                                                                   
+///                                                                                                     
+/// Implementation of neutrino oscillations in matter in a                                                
+/// three-neutrino framework with LIV as modelled by the SME.                                               
+/// The SME coefficients are included up to the 8th order.                                            
+///                                                                                                        
+/// This developement is part of the QGRANT project with                                                   
+/// ID: 101068013,                                                                                        
+/// founded by the HORIZON-MSCA-2021-PF-01-01 programme.                                               
+///                                                                                                     
+/// \author Nafis R. K. Chowdhury - nrkhanchowdhury\@km3net.de                                             
+/// \author Joao Coelho - jcoelho\@apc.in2p3.fr                                                           
+/// \author Alba Domi - alba.domi\@fau.de                       
+///////////////////////////////////////////////////////////////////////////////  
 
 #include "PMNS_LIV.h"
 #include "MatrixDecomp/zheevh3.h"
@@ -20,82 +25,42 @@ using namespace OscProb;
 
 using namespace std;
 
-
-//......................................................................
-///
-/// Constructor. \sa PMNS_Base::PMNS_Base
-///
+//...................................................................... 
+/// Constructor. 
 /// This class is restricted to 3 neutrino flavours.
+/// By default, all LIV coefficients are set to zero.
 ///
-//PMNS_LIV::PMNS_LIV() : PMNS_Fast(), faT(), fcTT()
 PMNS_LIV::PMNS_LIV() : PMNS_Fast()
 {
   SetStdPath();
-  SetLIV(0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.);
+
+  for(int dim = 3; dim <= 7; dim += 2){
+    SetaT(0,0, dim, 0, 0);
+    SetaT(1,1, dim, 0, 0);
+    SetaT(2,2, dim, 0, 0);
+    SetaT(0,1, dim, 0, 0);
+    SetaT(0,2, dim, 0, 0);
+    SetaT(1,2, dim, 0, 0);
+  }
+
+  for(int dim = 4; dim <= 8; dim += 2){
+    SetcT(0,0, 4, 0, 0);
+    SetcT(1,1, 4, 0, 0);
+    SetcT(2,2, 4, 0, 0);
+    SetcT(0,1, 4, 0, 0);
+    SetcT(0,2, 4, 0, 0);
+    SetcT(1,2, 4, 0, 0);
+  }
 }
 
 //......................................................................
 ///
-/// Nothing to clean.
+/// Distructor.
 ///
 PMNS_LIV::~PMNS_LIV(){}
 
 //......................................................................
-///
-/// Set all LIV parameters at once.
-///
-/// @param aT_ee          - The absolute value of the parameter aT_ee
-/// @param aT_mumu        - The absolute value of the parameter aT_mumu
-/// @param aT_tautau      - The absolute value of the parameter aT_tautau
-/// @param aT_emu         - The absolute value of the parameter aT_emu
-/// @param aT_etau        - The absolute value of the parameter aT_etau
-/// @param aT_mutau       - The absolute value of the parameter aT_mutau
-/// @param delta_aT_emu   - The phase of the complex parameter aT_emu in radians
-/// @param delta_aT_etau  - The phase of the complex parameter aT_etau in radians
-/// @param delta_aT_mutau - The phase of the complex parameter aT_mutau in radians
-/// @param cT_ee          - The absolute value of the parameter cT_ee
-/// @param cT_mumu        - The absolute value of the parameter cT_mumu
-/// @param cT_tautau      - The absolute value of the parameter cT_tautau
-/// @param cT_emu         - The absolute value of the parameter cT_emu
-/// @param cT_etau        - The absolute value of the parameter cT_etau
-/// @param cT_mutau       - The absolute value of the parameter cT_mutau
-/// @param delta_cT_emu   - The phase of the complex parameter cT_emu in radians
-/// @param delta_cT_etau  - The phase of the complex parameter cT_etau in radians
-/// @param delta_cT_mutau - The phase of the complex parameter cT_mutau in radians
-///
-void PMNS_LIV::SetLIV(double aT_ee,        double aT_mumu,       double aT_tautau,
-                      double aT_emu,       double aT_etau,       double aT_mutau,
-                      double cT_ee,        double cT_mumu,       double cT_tautau,
-                      double cT_emu,       double cT_etau,       double cT_mutau,
-                      double delta_aT_emu, double delta_aT_etau, double delta_aT_mutau,
-                      double delta_cT_emu, double delta_cT_etau, double delta_cT_mutau)
-{
-
-  SetaT(0,0, aT_ee, 0);
-  SetaT(1,1, aT_mumu, 0);
-  SetaT(2,2, aT_tautau, 0);
-
-  SetaT(0,1, aT_emu, delta_aT_emu);
-  SetaT(0,2, aT_etau, delta_aT_etau);
-  SetaT(1,2, aT_mutau, delta_aT_mutau);
-
-  SetcT(0,0, cT_ee, 0);
-  SetcT(1,1, cT_mumu, 0);
-  SetcT(2,2, cT_tautau, 0);
-
-  SetcT(0,1, cT_emu, delta_cT_emu);
-  SetcT(0,2, cT_etau, delta_cT_etau);
-  SetcT(1,2, cT_mutau, delta_cT_mutau);
-
-
-}
-
-//......................................................................
-///
-/// Set any given aT parameter.
-///
-/// This will check if value is changing to keep track of whether
-/// the eigensystem needs to be recomputed.
+/// Set any given aT parameter of a chosen dimension.
 ///
 /// Flavours are:\n
 /// - 0 = nue, 1 = numu, 2 = nutau
@@ -103,12 +68,13 @@ void PMNS_LIV::SetLIV(double aT_ee,        double aT_mumu,       double aT_tauta
 /// Requires that flvi < flvj. Will notify you if input is wrong.
 /// If flvi > flvj, will assume reverse order and swap flvi and flvj.
 ///
-/// @param flvi  - The first flavour index
-/// @param flvj  - The second flavour index
-/// @param val   - The absolute value of the parameter
-/// @param phase - The complex phase of the parameter in radians
+/// @param flvi  - First flavour index
+/// @param flvj  - Second flavour index
+/// @param dim   - Dimension of the coefficient: can be 3,5,7.
+/// @param val   - Absolute value of the parameter
+/// @param phase - Complex phase of the parameter in radians
 ///
-void PMNS_LIV::SetaT(int flvi, int flvj, double val, double phase){
+void PMNS_LIV::SetaT(int flvi, int flvj, int dim, double val, double phase){
 
   if(flvi > flvj){
     cout << "First argument should be smaller or equal to second argument" << endl;
@@ -122,40 +88,43 @@ void PMNS_LIV::SetaT(int flvi, int flvj, double val, double phase){
     cout << " neutrinos. Doing nothing." << endl;
     return;
   }
+  if(dim != 3 && dim != 5 && dim != 7){
+    cout << "aT coefficient dimension should be an odd value, starting from 3\n";
+    return;
+  }
 
-  complexD h = val;
+  int pos = dim == 3 ? 0: dim == 5 ? 1 : dim == 7 ? 2 : 4;
+  complexD h[3];
+  h[pos] = val;
 
-  if(flvi != flvj) h *= complexD(cos(phase), sin(phase));
+  if(flvi != flvj) h[pos] *= complexD(cos(phase), sin(phase));
 
-  bool isSame = (faT[flvi][flvj] == h);
+  bool isSame = (faT[flvi][flvj][pos] == h[pos]);
 
   if(!isSame) ClearCache();
 
   fGotES *= isSame;
 
-  faT[flvi][flvj] = h;
+  faT[flvi][flvj][pos] = h[pos];
 
 }
 
-//......................................................................
-///
-/// Set any given cT parameter.
-///
-/// This will check if value is changing to keep track of whether
-/// the eigensystem needs to be recomputed.
-///
-/// Flavours are:\n
-/// - 0 = nue, 1 = numu, 2 = nutau
-///
-/// Requires that flvi < flvj. Will notify you if input is wrong.
-/// If flvi > flvj, will assume reverse order and swap flvi and flvj.
-///
-/// @param flvi  - The first flavour index
-/// @param flvj  - The second flavour index
-/// @param val   - The absolute value of the parameter
-/// @param phase - The complex phase of the parameter in radians
-///
-void PMNS_LIV::SetcT(int flvi, int flvj, double val, double phase){
+//......................................................................                                  
+/// Set any given cT parameter of a chosen dimension.                         
+///                                                                                                       
+/// Flavours are:\n                                                                                            
+/// - 0 = nue, 1 = numu, 2 = nutau                                                                         
+///                                                                                                        
+/// Requires that flvi < flvj. Will notify you if input is wrong.                                           
+/// If flvi > flvj, will assume reverse order and swap flvi and flvj.                                      
+///                                                                                                         
+/// @param flvi  - First flavour index                                                                          
+/// @param flvj  - Second flavour index                                                                      
+/// @param dim   - Dimension of the coefficient: can be 4,6,8.      
+/// @param val   - Absolute value of the parameter                                                        
+/// @param phase - Complex phase of the parameter in radians                                         
+///                                                                    
+void PMNS_LIV::SetcT(int flvi, int flvj, int dim, double val, double phase){
 
   if(flvi > flvj){
     cout << "First argument should be smaller or equal to second argument" << endl;
@@ -169,24 +138,30 @@ void PMNS_LIV::SetcT(int flvi, int flvj, double val, double phase){
     cout << " neutrinos. Doing nothing." << endl;
     return;
   }
+  if(dim != 4 && dim != 6 && dim != 8){
+    cout << "cT coefficient dimension should be an even value, starting from 4\n";
+    return;
+  }
 
-  complexD h = val;
+  int pos = dim == 4 ? 0: dim == 6 ? 1 : dim == 8 ? 2 : 4;
+  complexD h[3];
+  h[pos] = val;
 
-  if(flvi != flvj) h *= complexD(cos(phase), sin(phase));
+  if(flvi != flvj) h[pos] *= complexD(cos(phase), sin(phase));
 
-  bool isSame = (fcT[flvi][flvj] == h);
+  bool isSame = (fcT[flvi][flvj][pos] == h[pos]);
 
   if(!isSame) ClearCache();
 
   fGotES *= isSame;
 
-  fcT[flvi][flvj] = h;
+  fcT[flvi][flvj][pos] = h[pos];
 
 }
 
 //......................................................................
 ///
-/// Get any given aT parameter.
+/// Get any given aT parameter of a chosen dimension.  
 ///
 /// Flavours are:\n
 /// - 0 = nue, 1 = numu, 2 = nutau
@@ -196,8 +171,9 @@ void PMNS_LIV::SetcT(int flvi, int flvj, double val, double phase){
 ///
 /// @param flvi  - The first flavour index
 /// @param flvj  - The second flavour index
+/// @param dim   - Dimension of the coefficient: can be 3,5,7.  
 ///
-complex<double> PMNS_LIV::GetaT(int flvi, int flvj){
+complex<double> PMNS_LIV::GetaT(int flvi, int flvj, int dim){
 
   if(flvi > flvj){
     cout << "First argument should be smaller or equal to second argument" << endl;
@@ -211,14 +187,20 @@ complex<double> PMNS_LIV::GetaT(int flvi, int flvj){
     cout << " neutrinos. Returning 0." << endl;
     return zero;
   }
+  if(dim != 3 && dim != 5 && dim != 7){
+    cout << "aT coefficient dimension should be an odd value, starting from 3\n";
+    return zero;
+  }
 
-  return faT[flvi][flvj];
+  int pos = dim == 3 ? 0: dim == 5 ? 1 : dim == 7 ? 2 : 4;
+  
+  return faT[flvi][flvj][pos];
 
 }
 
 //......................................................................
 ///
-/// Get any given cT parameter.
+/// Get any given cT parameter of a chosen dimension.
 ///
 /// Flavours are:\n
 /// - 0 = nue, 1 = numu, 2 = nutau
@@ -228,8 +210,9 @@ complex<double> PMNS_LIV::GetaT(int flvi, int flvj){
 ///
 /// @param flvi  - The first flavour index
 /// @param flvj  - The second flavour index
+/// @param dim   - Dimension of the coefficient: can be 4,6,8. 
 ///
-complex<double> PMNS_LIV::GetcT(int flvi, int flvj){
+complex<double> PMNS_LIV::GetcT(int flvi, int flvj, int dim){
 
   if(flvi > flvj){
     cout << "First argument should be smaller or equal to second argument" << endl;
@@ -243,112 +226,17 @@ complex<double> PMNS_LIV::GetcT(int flvi, int flvj){
     cout << " neutrinos. Returning 0." << endl;
     return zero;
   }
-
-  return fcT[flvi][flvj];
+  if(dim != 4 && dim != 6 && dim != 8){
+    cout << "cT coefficient dimension should be an even value, starting from 4\n";
+    return zero;
+  }
+  
+  int pos = dim == 4 ? 0: dim == 6 ? 1 : dim == 8 ? 2 : 4;
+  
+  return fcT[flvi][flvj][pos];
 
 }
 
-//......................................................................
-///
-/// Set aT_ee parameter
-///
-/// @param a   - The absolute value of the parameter
-///
-void PMNS_LIV::SetaT_ee(double a){ SetaT(0,0, a, 0); }
-
-//......................................................................
-///
-/// Set aT_mumu parameter
-///
-/// @param a   - The absolute value of the parameter
-///
-void PMNS_LIV::SetaT_mumu(double a){ SetaT(1,1, a, 0); }
-
-//......................................................................
-///
-/// Set aT_tautau parameter
-///
-/// @param a   - The absolute value of the parameter
-///
-void PMNS_LIV::SetaT_tautau(double a){ SetaT(2,2, a, 0); }
-
-//......................................................................
-///
-/// Set aT_emu parameter
-///
-/// @param a   - The absolute value of the parameter
-/// @param phi - The complex phase of the parameter in radians
-///
-void PMNS_LIV::SetaT_emu(double a, double phi){ SetaT(0,1, a, phi); }
-
-//......................................................................
-///
-/// Set aT_etau parameter
-///
-/// @param a   - The absolute value of the parameter
-/// @param phi - The complex phase of the parameter in radians
-///
-void PMNS_LIV::SetaT_etau(double a, double phi){ SetaT(0,2, a, phi); }
-
-//......................................................................
-///
-/// Set aT_mutau parameter
-///
-/// @param a   - The absolute value of the parameter
-/// @param phi - The complex phase of the parameter in radians
-///
-void PMNS_LIV::SetaT_mutau(double a, double phi){ SetaT(1,2, a, phi); }
-
-//......................................................................
-///
-/// Set cT_ee parameter
-///
-/// @param a   - The absolute value of the parameter
-///
-void PMNS_LIV::SetcT_ee(double a){ SetcT(0,0, a, 0); }
-
-//......................................................................
-///
-/// Set cT_mumu parameter
-///
-/// @param a   - The absolute value of the parameter
-///
-void PMNS_LIV::SetcT_mumu(double a){ SetcT(1,1, a, 0); }
-
-//......................................................................
-///
-/// Set cT_tautau parameter
-///
-/// @param a   - The absolute value of the parameter
-///
-void PMNS_LIV::SetcT_tautau(double a){ SetcT(2,2, a, 0); }
-
-//......................................................................
-///
-/// Set cT_emu parameter
-///
-/// @param a   - The absolute value of the parameter
-/// @param phi - The complex phase of the parameter in radians
-///
-void PMNS_LIV::SetcT_emu(double a, double phi){ SetcT(0,1, a, phi); }
-
-//......................................................................
-///
-/// Set cT_etau parameter
-///
-/// @param a   - The absolute value of the parameter
-/// @param phi - The complex phase of the parameter in radians
-///
-void PMNS_LIV::SetcT_etau(double a, double phi){ SetcT(0,2, a, phi); }
-
-//......................................................................
-///
-/// Set cT_mutau parameter
-///
-/// @param a   - The absolute value of the parameter
-/// @param phi - The complex phase of the parameter in radians
-///
-void PMNS_LIV::SetcT_mutau(double a, double phi){ SetcT(1,2, a, phi); }
 
 //......................................................................
 ///
@@ -362,16 +250,29 @@ void PMNS_LIV::UpdateHam()
 {
 
   double lv = 2 * kGeV2eV*fEnergy;           // 2*E in eV
-  //double fEnergy_ev =     kGeV2eV*fEnergy;           // E in eV
 
-  double kr2GNe   = kK2*M_SQRT2*kGf*fPath.density;
+  double kr2GNe = kK2*M_SQRT2*kGf*fPath.density;
   kr2GNe *=  fPath.zoa; // Matter potential in eV
 
   // Finish build Hamiltonian in matter with dimension of eV
   for(int i=0;i<fNumNus;i++){
     for(int j=i;j<fNumNus;j++){
-      if(!fIsNuBar) fHam[i][j] = fHms[i][j]/lv + kGeV2eV*(faT[i][j] - (4.*fEnergy / 3.)*fcT[i][j]);
-      else          fHam[i][j] = conj(fHms[i][j]/lv + kGeV2eV*(- faT[i][j] - (4.*fEnergy / 3.)*fcT[i][j])); // +/- faTs for nu/nubar (CPTodd) 
+      if(!fIsNuBar){
+	fHam[i][j] = fHms[i][j]/lv + kGeV2eV*(faT[i][j][0]
+					      -4./3. * fEnergy * fcT[i][j][0]
+					      +std::pow(fEnergy, 2) * faT[i][j][1]
+					      -4./3. * std::pow(fEnergy, 3) * fcT[i][j][1]
+					      +std::pow(fEnergy, 4) * faT[i][j][2]
+					      -4./3. * std::pow(fEnergy, 5) * fcT[i][j][2]);
+
+      } else {
+	fHam[i][j] = conj(fHms[i][j]/lv + kGeV2eV*(-faT[i][j][0]
+						   -4./3. * fEnergy * fcT[i][j][0]
+						   -std::pow(fEnergy, 2) * faT[i][j][1]
+						   -4./3. * std::pow(fEnergy, 3) * fcT[i][j][1]
+						   -std::pow(fEnergy, 4) * faT[i][j][2]
+						   -4./3. * std::pow(fEnergy, 5) * fcT[i][j][2]));
+      }
     }
   }
 
--- PMNS_LIV.h
+++ PMNS_LIV.h
@@ -1,11 +1,17 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// class OscProb::PMNS_LIV
 ///
-/// Brief Implementation of oscillations of neutrinos in matter in a
-///        three-neutrino framework with LIV.
+/// Implementation of neutrino oscillations in matter in a
+/// three-neutrino framework with LIV as modelled by the SME.
+/// The SME coefficients are included up to the 8th order.
+///
+/// This developement is part of the QGRANT project with
+/// ID: 101068013,
+/// founded by the HORIZON-MSCA-2021-PF-01-01 programme.
 ///
 /// \author Nafis R. K. Chowdhury - nrkhanchowdhury\@km3net.de 
 /// \author Joao Coelho - jcoelho\@apc.in2p3.fr
+/// \author Alba Domi - alba.domi\@fau.de
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef PMNS_LIV_H
@@ -21,52 +27,31 @@ namespace OscProb {
     PMNS_LIV();          ///< Constructor
     virtual ~PMNS_LIV(); ///< Destructor
 
-    /// Set any given LIV parameter
-    virtual void SetaT(int flvi, int flvj, double val, double phase);
-    virtual void SetcT(int flvi, int flvj, double val, double phase);
-
-    /// Set the LIV parameters all at once
-    void SetLIV(double aT_ee,     double aT_mumu,      double aT_tautau,
-            double aT_emu,     double aT_etau,      double aT_mutau,
-            double cT_ee,     double CT_mumu,      double CT_tautau,
-            double cT_emu,     double cT_etau,      double cT_mutau,
-            double delta_aT_emu=0, double delta_aT_etau=0, double delta_aT_mutau=0,
-	    double delta_cT_emu=0, double delta_cT_etau=0, double delta_cT_mutau=0);
-
-    //Set diagonal LIV pars
-    virtual void SetaT_ee    (double a); ///< Set eps_ee parameter
-    virtual void SetaT_mumu  (double a); ///< Set eps_mumu parameter
-    virtual void SetaT_tautau(double a); ///< Set eps_tautau parameter
+    /// Set any given LIV parameter of a chosen dimension.
+    /// The flavour convention is:
+    ///   e   -> 0
+    ///   mu  -> 1
+    ///   tau -> 2
+    virtual void SetaT(int flvi, int flvj, int dim, double val, double phase);
+    virtual void SetcT(int flvi, int flvj, int dim, double val, double phase);
     
-    virtual void SetcT_ee    (double a); ///< Set eps_ee parameter
-    virtual void SetcT_mumu  (double a); ///< Set eps_mumu parameter
-    virtual void SetcT_tautau(double a); ///< Set eps_tautau parameter
-
-
-    /// Set diagonal LIV pars
-    virtual void SetaT_emu  (double a, double phi);
-    virtual void SetaT_etau (double a, double phi);
-    virtual void SetaT_mutau(double a, double phi);
-
-
-    virtual void SetcT_emu  (double a, double phi);
-    virtual void SetcT_etau (double a, double phi);
-    virtual void SetcT_mutau(double a, double phi);
-
-    /// Get any given LIV par
-    virtual complexD GetaT(int flvi, int flvj);
-    virtual complexD GetcT(int flvi, int flvj);
+    /// Get any given LIV parameter of a chosen dimension.
+    /// The flavour convention is:                                                         
+    ///   e   -> 0                                                                                        
+    ///   mu  -> 1                                                                                        
+    ///   tau -> 2    
+    virtual complexD GetaT(int flvi, int flvj, int dim);
+    virtual complexD GetcT(int flvi, int flvj, int dim);
 
   protected:
 
     /// Build the full Hamiltonian
     virtual void UpdateHam();
 
-    complexD faT[3][3]; ///< Stores each LIV1 parameter
-    complexD fcT[3][3]; ///< Stores each LIV1 parameter
+    complexD faT[3][3][3]; ///< Stores each aT LIV parameter of dimension 3,5,7
+    complexD fcT[3][3][3]; ///< Stores each cT LIV parameter of dimension 4,6,8
 
   };
 
 }
 #endif
-////////////////////////////////////////////////////////////////////////
