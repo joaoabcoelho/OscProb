b9000b25ba25fdd8c605ca8a28e623ab5b4631ca
RPestes <94020360+RPestes@users.noreply.github.com>
2023-02-24 10:49:33 +0100

    Add EarthTables and defaults
    This is where the binned Earth model tables should be kept for the EarthModel class to use.

---
+++ EarthTables/model_defaults.hpp
@@ -0,0 +1,3 @@
+#include <string>
+const std::string MODEL_DIR = "/home/work/Desktop/AsymmetricEarth/EarthTables";
+const std::string PREM3D_DEFAULT = "/home/work/Desktop/AsymmetricEarth/EarthTables/prem_default.txt";
