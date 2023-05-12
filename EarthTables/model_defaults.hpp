574c57d696e43dcec935922eb003a97fb3cbbf9c
RPestes <94020360+RPestes@users.noreply.github.com>
2023-05-12 15:38:13 +0200

    Remove model_defaults.hpp
    This file should not exist in the repository: it needs to be created with the makefile (maybe as a part of prem_default.hpp, instead of separately).

--- EarthTables/model_defaults.hpp
+++
@@ -1,3 +0,0 @@
-#include <string>
-const std::string MODEL_DIR = "/home/work/Desktop/AsymmetricEarth/EarthTables";
-const std::string PREM3D_DEFAULT = "/home/work/Desktop/AsymmetricEarth/EarthTables/prem_default.txt";
