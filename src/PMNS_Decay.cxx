d83adf326b46a2777ef8c06ecf73d41b75977eb3
joaoabcoelho <joaoabcoelho@gmail.com>
2023-08-04 12:55:24 +0200

    Some style improvements

--- src/PMNS_Decay.cxx
+++ src/PMNS_Decay.cxx
@@ -1,30 +1,30 @@
-////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 ///
 /// Implementation of oscillations of neutrinos in matter in a
-/// three-neutrino framework with Neutrino Decay in the 3rd state. 
+/// three-neutrino framework with Neutrino Decay in the 3rd state.
 ///
 /// This class expands the PMNS_Fast class including the decay of the 
 /// second and third mass state of the neutrino through a decay constant
 /// alpha_i=m_i/tau_i (eV^2), where m_i is the mass in the restframe and
 /// tau_i is the lifetime in the restframe.
 ///
-/// Diagonalization of the matrix is done using the Eigen library. 
+/// Diagonalization of the matrix is done using the Eigen library.
 ///
-/// \author Victor Carretero - vcarretero\@km3net.de  
+/// \author Victor Carretero - vcarretero\@km3net.de
 /// \author Joao Coelho - jcoelho\@apc.in2p3.fr
-////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 
-#include "complexsolver.h"
 #include <unsupported/Eigen/MatrixFunctions>
 
+#include "complexsolver.h"
 #include "PMNS_Decay.h"
 
 using namespace OscProb;
 using namespace std;
 
-//......................................................................
+//.............................................................................
 ///
 /// Constructor. \sa PMNS_Base::PMNS_Base
 ///
@@ -32,75 +32,92 @@ using namespace std;
 ///
 PMNS_Decay::PMNS_Decay() : PMNS_Base() {
 
-  falpha         = vectorD(fNumNus, 0);
+  fAlpha         = vectorD(fNumNus, 0);
   fHam           = Eigen::MatrixXcd(fNumNus, fNumNus);
   fHd            = matrixC(fNumNus, vectorC(fNumNus,zero));
 
 }
 
-//......................................................................
+//.............................................................................
 ///
 /// Nothing to clean.
 ///
 PMNS_Decay::~PMNS_Decay(){}
 
-//......................................................................
+//.............................................................................
 ///
 /// Set all mixing parameters at once.
+///
 /// @param th12    - The value of the mixing angle theta_12
 /// @param th23    - The value of the mixing angle theta_23
 /// @param th13    - The value of the mixing angle theta_13
 /// @param deltacp - The value of the CP phase delta_13
 ///
-void PMNS_Decay::SetMix(double th12, double th23, double th13, double deltacp) 
+void PMNS_Decay::SetMix(double th12, double th23, double th13, double deltacp)
 {
+
   SetAngle(1,2, th12);
   SetAngle(1,3, th13);
   SetAngle(2,3, th23);
   SetDelta(1,3, deltacp);
+
 }
 
-//......................................................................                                                       
-///     Setting Alpha3 parameter, it must be possitive, and units are eV^2 .
-///     Alpha3=m3/tau3, mass and lifetime of the third state in the restframe 
-void PMNS_Decay::SetAlpha3(double alpha3) 
+//.............................................................................
+///
+/// Setting Alpha3 parameter, it must be possitive, and units are eV^2.
+///
+/// @param alpha3 = m3/tau3, mass and lifetime of the 3rd state in the restframe
+///
+void PMNS_Decay::SetAlpha3(double alpha3)
 {
+
   if(alpha3<0){
     cerr << "WARNING: Alpha3 must be positive. Doing nothing." << endl;
     return;
   }
 
-  fBuiltHms *= (falpha[2] == alpha3);
-  falpha[2] = alpha3;
+  fBuiltHms *= (fAlpha[2] == alpha3);
+  fAlpha[2] = alpha3;
+
 }
 
-//......................................................................                                              
-///     Setting Alpha2 parameter, it must be possitive, and units are eV^2 .
-///     Alpha2=m2/tau2, mass and lifetime of the second state in the restframe
+//.............................................................................
+///
+/// Setting Alpha2 parameter, it must be possitive, and units are eV^2.
+///
+/// @param alpha2 = m2/tau2, mass and lifetime of the 2nd state in the restframe
+///
 void PMNS_Decay::SetAlpha2(double alpha2)
 {
+
   if(alpha2<0){
     cerr << "WARNING: Alpha2 must be positive. Doing nothing." << endl;
     return;
   }
 
-  fBuiltHms *= (falpha[1] == alpha2);
-  falpha[1] = alpha2;
+  fBuiltHms *= (fAlpha[1] == alpha2);
+  fAlpha[1] = alpha2;
+
 }
 
-//......................................................................
+//.............................................................................
 ///
-/// Reimplement SetIsNuBar to also rebuild hamiltonian..
+/// Reimplement SetIsNuBar to also rebuild hamiltonian.
+///
+/// @param isNuBar - true if antineutrinos
 ///
 void PMNS_Decay::SetIsNuBar(bool isNuBar)
 {
+
   // Check if value is actually changing
   fGotES *= (fIsNuBar == isNuBar);
   fBuiltHms *= (fIsNuBar == isNuBar);
   fIsNuBar = isNuBar;
+
 }
 
-//......................................................................
+//.............................................................................
 ///
 /// Set both mass-splittings at once.
 ///
@@ -110,43 +127,54 @@ void PMNS_Decay::SetIsNuBar(bool isNuBar)
 /// @param dm21 - The solar mass-splitting Dm_21 
 /// @param dm32 - The atmospheric mass-splitting Dm_32 
 ///
-void PMNS_Decay::SetDeltaMsqrs(double dm21, double dm32) 
+void PMNS_Decay::SetDeltaMsqrs(double dm21, double dm32)
 {
+
   SetDm(2, dm21);
   SetDm(3, dm32 + dm21);
+
 }
 
-//
-/// Get alpha3
-double PMNS_Decay::GetAlpha3() 
+//.............................................................................
+///
+/// Get alpha3 parameter
+///
+/// @return alpha3
+///
+double PMNS_Decay::GetAlpha3()
 {
-  return falpha[2];
+  return fAlpha[2];
 }
 
-/// Get alpha2
+//.............................................................................
+///
+/// Get alpha2 parameter
+///
+/// @return alpha2
+///
 double PMNS_Decay::GetAlpha2()
 {
-  return falpha[1];
+  return fAlpha[1];
 }
 
 
-//......................................................................
+//.............................................................................
 ///
 ///  Implement building decay hamiltonian
 ///
-void PMNS_Decay::BuildHam()
+void PMNS_Decay::BuildHms()
 {
 
   // Check if anything changed
   if(fBuiltHms) return;
-   
+
   // Tag to recompute eigensystem
   fGotES = false;
-  
+
   for(int j=0; j<fNumNus; j++){
     // Set mass splitting
     fHms[j][j] = fDm[j];
-    fHd[j][j] = falpha[j];
+    fHd[j][j] = fAlpha[j];
     // Reset off-diagonal elements
     for(int i=0; i<j; i++){
       fHms[i][j] = 0;
@@ -172,14 +200,14 @@ void PMNS_Decay::BuildHam()
     }
   }
 
-  const complexD numi(0.0,1.0);  
+  const complexD numi(0.0,1.0);
   ///Construct the total Hms+Hd
   for(int j=0; j<fNumNus; j++){
     for(int i=0; i<fNumNus; i++){
       fHms[i][j] = fHms[i][j] - numi*fHd[i][j];
     }
-  }  
- 
+  }
+
   // Clear eigensystem cache
   //ClearCache();
 
@@ -189,7 +217,7 @@ void PMNS_Decay::BuildHam()
 }
 
 
-//......................................................................
+//.............................................................................
 ///
 /// Build the full Hamiltonian in matter.
 ///
@@ -211,13 +239,13 @@ void PMNS_Decay::UpdateHam()
       fHam(i,j) = fHms[i][j]/lv;
     }
   }
- 
+
   if(!fIsNuBar) fHam(0,0) += kr2GNe;
   else          fHam(0,0) -= kr2GNe;
-  
+
 }
 
-//......................................................................
+//.............................................................................
 ///
 /// Solve the full Hamiltonian for eigenvalues.
 ///
@@ -230,7 +258,7 @@ void PMNS_Decay::SolveHam()
 {
 
   // Build Hamiltonian
-  BuildHam();
+  BuildHms();
 
   // Check if anything changed  
   if(fGotES) return;
@@ -250,9 +278,11 @@ void PMNS_Decay::SolveHam()
 
 }
 
-//.....................................................................
+//.............................................................................
 ///
 /// Propagate the neutrino through a constant density path
+///
+/// @param p - The neutrino path
 ///
 void PMNS_Decay::PropagatePath(NuPath p)
 {
@@ -261,7 +291,7 @@ void PMNS_Decay::PropagatePath(NuPath p)
   SetCurPath(p);
 
   // Build Hamiltonian
-  BuildHam();
+  BuildHms();
   UpdateHam();
 
   double LengthIneV = kKm2eV * p.length;
@@ -283,7 +313,7 @@ void PMNS_Decay::PropagatePath(NuPath p)
   for(int i=0;i<fNumNus;i++){
     fNuState[i] = fBuffer[i];
   }
-  
+
 }
 
 
