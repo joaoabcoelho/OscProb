0f9cbddae1afb18ad2b983067eaf45ac50dd94b7
joaoabcoelho <joaoabcoelho@gmail.com>
2023-08-10 10:39:13 +0200

    chore: Add check for callgrind reader and make executable

--- test/profile/run_callgrind.sh
+++ test/profile/run_callgrind.sh
@@ -6,10 +6,21 @@ else
   MODEL=$1
 fi
 
+make
+
 echo "Running profiler over PMNS_$MODEL"
 
 valgrind --tool=callgrind --callgrind-out-file=callgrind.out -q ./bin/stress $MODEL
 
 echo "Open profiler output"
 
-kcachegrind callgrind.out
+if command -v kcachegrind &> /dev/null
+then
+  kcachegrind callgrind.out
+elif command -v callgrind_annotate &> /dev/null
+then
+  callgrind_annotate callgrind.out
+else
+  echo "Neither kcachegrind nor callgrind_annotate executables found"
+  echo "Cannot open callgrind output"
+fi
