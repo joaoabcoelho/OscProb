46133402f8d9e23b9b5c8f5def4595f979b65c68
joaoabcoelho <joaoabcoelho@gmail.com>
2025-11-19 16:18:28 +0100

    doc: Flag as obsolete

--- inc/PMNS_Avg.h
+++ inc/PMNS_Avg.h
@@ -1,19 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// \class OscProb::PMNS_Avg
 ///
-/// \brief Implementation of oscillations of neutrinos in matter in a
-///        three-neutrino framework with a first order Taylor expansion.
-///
-/// This class expands the PMNS_Fast class including the use of a first order
-/// Taylor expansion to calculate the average on bins faster.
-///
-/// The model assumes a first order expansion over neutrino energy and angle
-/// for both dynamical variables at the same time or for only one.
-///
-/// This is the first version of this class. A second version will be release
-/// with a better implementation with the other classes.
-///
-/// Reference: https://doi.org/10.48550/arXiv.2308.00037
+/// \brief OBSOLETE: Exactly the same as PMNS_Fast
 ///
 /// \sa PMNS_Fast
 ///
