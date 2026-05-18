257d1a9cae98ca07c7c2148b817fae8e403d0f14
Alba Domi <adomi@km3net.de>
2026-05-19 00:13:53 +0200

    fix issue with ci testing

--- test/MakeTestSamples.C
+++ test/MakeTestSamples.C
@@ -7,8 +7,8 @@ void MakeTestSamples(vector<string> models = {}){
 
   for(int i=0; i<models.size(); i++){
 
-    TString filename = "data/PMNS_"+models[i]+"_test_values.root";
-    if(!gSystem->AccessPathName(filename)) continue;
+    TString filename = "PMNS_"+models[i]+"_test_values.root";
+    if(!gSystem->AccessPathName("data/"+filename)) continue;
 
     OscProb::PMNS_Base* p = GetModel(models[i]);
     SaveTestFile(p, filename);
